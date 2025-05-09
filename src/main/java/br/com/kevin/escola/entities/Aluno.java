63add0d8d8cf1fbbeff596cd3f56245fe76ed660
Kevin Moura <kevinsmoura@hotmail.com>
2025-05-09 09:28:41 -0300

    Audity data

--- src/main/java/br/com/kevin/escola/entities/Aluno.java
+++ src/main/java/br/com/kevin/escola/entities/Aluno.java
@@ -16,8 +16,9 @@ public class Aluno {
     private String cpf;
     private Instant dataNascimento;
 
-    @Column()
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant createdAt;
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
     private Instant updatedAt;
 
     public Aluno(Long id, String name, String cpf, Instant dataNascimento) {
@@ -56,6 +57,24 @@ public class Aluno {
 
     public void setCpf(String cpf) {
         this.cpf = cpf;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    @PrePersist
+    public void preCreate(){
+        this.createdAt = Instant.now();
+    }
+
+    @PreUpdate
+    public void preUpdate(){
+        this.updatedAt = Instant.now();
     }
 
     @Override
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,7 +1,7 @@
-INSERT INTO tab_aluno (cpf, data_nascimento, name) values ('123.456.789-00', '1996-02-14', 'Kevin Moura');
-INSERT INTO tab_aluno (cpf, data_nascimento, name) values ('321.456.789-00', '1997-03-15', 'Beatriz Silva');
-INSERT INTO tab_aluno (cpf, data_nascimento, name) values ('231.456.789-00', '1998-04-16', 'João Costa');
-INSERT INTO tab_aluno (cpf, data_nascimento, name) values ('123.654.789-00', '1999-05-17', 'Maria Vieira');
-INSERT INTO tab_aluno (cpf, data_nascimento, name) values ('123.546.789-00', '2000-06-18', 'Lucas Sousa');
-INSERT INTO tab_aluno (cpf, data_nascimento, name) values ('123.456.987-00', '2001-07-19', 'Ronaldo Azevedo');
-INSERT INTO tab_aluno (cpf, data_nascimento, name) values ('123.456.978-00', '2002-08-20', 'Leticia Dias');
+INSERT INTO tab_aluno (cpf, data_nascimento, name, created_At) values ('123.456.789-00', '1996-02-14', 'Kevin Moura', NOW());
+INSERT INTO tab_aluno (cpf, data_nascimento, name, created_At) values ('321.456.789-00', '1997-03-15', 'Beatriz Silva', NOW());
+INSERT INTO tab_aluno (cpf, data_nascimento, name, created_At) values ('231.456.789-00', '1998-04-16', 'João Costa', NOW());
+INSERT INTO tab_aluno (cpf, data_nascimento, name, created_At) values ('123.654.789-00', '1999-05-17', 'Maria Vieira', NOW());
+INSERT INTO tab_aluno (cpf, data_nascimento, name, created_At) values ('123.546.789-00', '2000-06-18', 'Lucas Sousa', NOW());
+INSERT INTO tab_aluno (cpf, data_nascimento, name, created_At) values ('123.456.987-00', '2001-07-19', 'Ronaldo Azevedo', NOW());
+INSERT INTO tab_aluno (cpf, data_nascimento, name, created_At) values ('123.456.978-00', '2002-08-20', 'Leticia Dias', NOW());
